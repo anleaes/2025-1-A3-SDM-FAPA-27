0bc24dedea6c88f60b1506d00ddecae59526d948
WesleyDev0101 <wesley.coldyto@gmail.com>
2025-06-26 14:55:33 -0300

    feat: IP release final

--- RecrutamentoSelecao/settings.py
+++ RecrutamentoSelecao/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-#2ewi1y1dazytk!o)p^!)n0l2gs^l!e(c742hixat)ll1he(@2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
